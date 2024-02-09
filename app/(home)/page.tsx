47dc6f10dfdf8cae634090707a6237404cc3dd18
victor oliveira <victor.in@icloud.com>
2024-02-09 03:05:40 -0300

    feat: add header

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+//import Image from "next/image";
+import Header from "@/app/_components/header";
 
 export default function Home() {
   return (
     <div>
-      <header />
+      <Header />
     </div>
   );
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,14 +1,16 @@
-import {CardContent} from "@/app/_components/ui/card";
+import {Card, CardContent} from "@/app/_components/ui/card";
 import {MenuIcon} from "lucide-react";
+import {Button} from "@/app/_components/ui/button";
+import Image from "next/image";
 
 const header = () => {
     return (
         <Card>
-            <CardContent>
-                <image src="logo.png" alt="FSW barber" height={22} width={120}/>
-                <button variant="outline" size="icon">
-                    <MenuIcon/>
-                </button>
+            <CardContent className="p-5 justify-between items-center flex flex-row">
+                <Image src="/logo.png" alt="FSW barber" height={18} width={120}/>
+                <Button variant="outline" size="icon" className="h-9 w-9">
+                    <MenuIcon size={18}/>
+                </Button>
             </CardContent>
         </Card>
     );
